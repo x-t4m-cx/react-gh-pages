e30d6bc545821907275f6cda8c458150991b6d31
x-t4m-cx <t4bakovm@yandex.ru>
2026-03-31 08:50:24 +0400

    add news pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import './App.css';
+import News from './pages/News';
 
 function App() {
   return (
@@ -15,12 +16,16 @@ function App() {
           <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>
             About
           </Link>
+          <Link to="/news" style={{ color: 'white', textDecoration: 'none' }}>
+            News
+          </Link>
         </nav>
 
         {/* Маршрутизация */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/News.js
@@ -0,0 +1,15 @@
+function News() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>News Page</h1>
+        <p>News 1</p>
+        <p>News 2</p>
+        <p>News 3</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default News;

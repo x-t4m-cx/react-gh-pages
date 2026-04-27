fb6c22fb6543f6974f4338f91b009a96180576dd
x-t4m-cx <t4bakovm@yandex.ru>
2026-04-27 11:10:57 +0400

    dd

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
 import './App.css';
-import News from './pages/News';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* Навигационное меню */}
-        <nav style={{ padding: '20px', backgroundColor: '#282c34', color: 'white' }}>
-          <Link to="/" style={{ color: 'white', marginRight: '20px', textDecoration: 'none' }}>
-            Home
-          </Link>
-          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>
-            About
-          </Link>
-          <Link to="/news" style={{ color: 'white', textDecoration: 'none' }}>
-            News
-          </Link>
-        </nav>
-
-        {/* Маршрутизация */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/news" element={<News />} />
-        </Routes>
+    <div className="app-container">
+      <div className="heart-container">
+        <div className="heart">❤️</div>
       </div>
-    </Router>
+      <div className="video-container">
+        
+      </div>
+    </div>
   );
 }
 
